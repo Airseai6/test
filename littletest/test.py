c042b8dfeddcaf65dc7077d19c24b55f93646bd5
Airsea <airseai6@126.com>
2019-01-17 01:30:05 +0800

    Signed-off-by: Airsea <airseai6@126.com> 学校编号文件

--- littletest/test.py
+++ littletest/test.py
@@ -10,6 +10,13 @@ import zipfile
 #     for file in filesname:
 #         # 将当前路径与当前路径下的文件名组合，就是当前文件的绝对路径
 #         azip.write(os.path.join(current_path[3:], file))
-import shutil
-# 第一个参数是归档文件名称，第二个参数是指定的格式，不仅是支持zip，第三个参数是要压缩文件/文件夹的路径
-shutil.make_archive('archive_name', 'zip', r'123')
+# import shutil
+# # 第一个参数是归档文件名称，第二个参数是指定的格式，不仅是支持zip，第三个参数是要压缩文件/文件夹的路径
+# shutil.make_archive('archive_name', 'zip', r'123')
+while True:
+   m = input("input: ")
+   if m:
+       with open("test.txt", "a") as f:
+           f.write(m + "\n")
+       continue
+   break
---
+++ school_index/school_index.py
@@ -0,0 +1,50 @@
+#! python3
+# -*- coding:utf-8 -*-
+from xpinyin import Pinyin
+
+
+def gen_id(input_file, nginx=8900, python=5800, webs=8330):
+    """
+    生成网站上的学校相关编号
+    """
+    with open(input_file, 'r')as f:
+        data = f.readlines()
+    data = [line.strip().split(',') for line in data]
+    school_list = []
+    p = Pinyin()
+
+    for i in range(len(data)):
+        school = {}
+        school['u_name'] = data[i][0]
+        school['a_name'] = data[i][1]
+        # 学校简称，学院单独处理
+        # abb = p.get_initials(school['u_name'], '').lower() + p.get_initials(school['a_name'], '').lower()
+        abb = p.get_initials(school['u_name'], '').lower()
+        school['Nginx HTTP'] = nginx + 100*i
+        school['Python'] = python + 100*i
+        school['MySQL'] = 3310
+        school['Redis'] = 5001
+        school['WebSocket'] = webs + 100*i
+        school['HTML'] = 'html_' + abb
+        school['DB'] = 'cloudlearn_' + abb
+        school_list.append(school)
+    # print(school_list)
+
+    with open('new_list.txt', 'a')as f:
+        for school in school_list:
+            content = '### CloudLearn ' + school['u_name'] + school['a_name'] + '\n\n' \
+                      + 'http://119.23.40.38:' + str(school['Nginx HTTP']) + '/\n\n' \
+                      + '    ' + str(school['Nginx HTTP']) + ' Nginx HTTP\n' \
+                      + '    ' + str(school['Python']) + ' Python\n' \
+                      + '    ' + str(school['MySQL']) + ' MySQL\n' \
+                      + '    ' + str(school['Redis']) + ' Redis\n' \
+                      + '    ' + str(school['WebSocket']) + ' WebSocket\n' \
+                      + '    ' + str(school['HTML']) + ' HTML\n' \
+                      + '    ' + str(school['DB']) + ' DB\n' \
+                      + '\n\n'
+            f.write(content)
+
+
+if __name__ == '__main__':
+    file = 'list.csv'
+    gen_id(file)
