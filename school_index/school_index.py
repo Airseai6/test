2a6fa4fed89e9ff736fcb8288d39ec57750b41cb
Airseai6 <1042536516@qq.com>
2019-01-17 18:55:53 +0800

    20190117 Signed-off-by: Airseai6 <1042536516@qq.com>

--- school_index/school_index.py
+++ school_index/school_index.py
@@ -29,18 +29,18 @@ def gen_id(input_file, nginx=8900, python=5800, webs=8330):
         school['DB'] = 'cloudlearn_' + abb
         school_list.append(school)
     # print(school_list)
-
+    l_chr = '\t'
     with open('new_list.txt', 'a')as f:
         for school in school_list:
             content = '### CloudLearn ' + school['u_name'] + school['a_name'] + '\n\n' \
                       + 'http://119.23.40.38:' + str(school['Nginx HTTP']) + '/\n\n' \
-                      + '    ' + str(school['Nginx HTTP']) + ' Nginx HTTP\n' \
-                      + '    ' + str(school['Python']) + ' Python\n' \
-                      + '    ' + str(school['MySQL']) + ' MySQL\n' \
-                      + '    ' + str(school['Redis']) + ' Redis\n' \
-                      + '    ' + str(school['WebSocket']) + ' WebSocket\n' \
-                      + '    ' + str(school['HTML']) + ' HTML\n' \
-                      + '    ' + str(school['DB']) + ' DB\n' \
+                      + l_chr + str(school['Nginx HTTP']) + ' Nginx HTTP\n' \
+                      + l_chr + str(school['Python']) + ' Python\n' \
+                      + l_chr + str(school['MySQL']) + ' MySQL\n' \
+                      + l_chr + str(school['Redis']) + ' Redis\n' \
+                      + l_chr + str(school['WebSocket']) + ' WebSocket\n' \
+                      + l_chr + str(school['HTML']) + ' HTML\n' \
+                      + l_chr + str(school['DB']) + ' DB\n' \
                       + '\n\n'
             f.write(content)
 
