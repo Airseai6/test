197ad3a976b789140a6c48c8703811986eed6a69
Airseai6 <1042536516@qq.com>
2019-03-16 10:31:12 +0800

    heliostat location
    Signed-off-by: Airseai6 <1042536516@qq.com>

--- test0.py
+++ test0.py
@@ -39,21 +39,28 @@
 # from datetime import datetime
 # print(datetime.now())
 
-from math import *
-a = asin(900/1575)
-a = a/pi*180
+# from math import *
+# a = asin(900/1575)
+# a = a/pi*180
+#
+# delta_h = 212
+# Dm = 20
+# r = 140
+# b = atan(delta_h/r)
+# # b = b/pi*180
+# delta_r = Dm*(1.009/b - 0.063 + 0.4803*b)
+# delta_z = Dm*(2.17 - 0.6589*b + 1.247*b*b)
+# theta = 2*asin(0.5*delta_z/r)
+# theta = theta/pi*180
+#
+# print('high_angle:', b)
+# print('delta_R:', delta_r)
+# print('delta_Z:', delta_z)
+# print('next_location:', (r, u"%.3f°"%theta))
 
-delta_h = 212
-Dm = 20
-r = 140
-b = atan(delta_h/r)
-# b = b/pi*180
-delta_r = Dm*(1.009/b - 0.063 + 0.4803*b)
-delta_z = Dm*(2.17 - 0.6589*b + 1.247*b*b)
-theta = 2*asin(0.5*delta_z/r)
-theta = theta/pi*180
 
-print('high_angle:', b)
-print('delta_R:', delta_r)
-print('delta_Z:', delta_z)
-print('next_location:', (r, u"%.3f°"%theta))
+import requests
+
+response = requests.get('https://www.baidu.com')
+# print(response.text)
+print(response.content.decode('utf-8'))
